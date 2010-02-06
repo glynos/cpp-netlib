a53e7e1bf337b58837e0e9228fe49c5416602da4
Dean Michael Berris <mikhailberis@gmail.com>
2010-02-06 16:23:59 +0800

    Refactoring http_message_test to test other tags for message types.

--- libs/network/test/http_message_test.cpp
+++ libs/network/test/http_message_test.cpp
@@ -13,36 +13,38 @@
 
 using namespace boost::network;
 
+typedef boost::mpl::list<tags::http_default_8bit_tcp_resolve, tags::http_default_8bit_udp_resolve, tags::http_keepalive_8bit_tcp_resolve, tags::http_keepalive_8bit_udp_resolve> tag_types;
+
 struct fixtures {
 };
 
 BOOST_FIXTURE_TEST_SUITE(http_message_test_suite, fixtures)
 
-BOOST_AUTO_TEST_CASE(request_constructor_test) {
-    http::client::request request("http://boost.org");
+BOOST_AUTO_TEST_CASE_TEMPLATE (request_constructor_test, T, tag_types) {
+    http::basic_request<T> request("http://boost.org");
     BOOST_CHECK_EQUAL ( request.host() , "boost.org" );
     BOOST_CHECK_EQUAL ( request.port() , 80u );
     BOOST_CHECK_EQUAL ( request.path() , "/" );
 }
 
-BOOST_AUTO_TEST_CASE(request_copy_constructor_test) {
-    http::client::request request("http://boost.org/handler.php");
+BOOST_AUTO_TEST_CASE_TEMPLATE (request_copy_constructor_test, T, tag_types) {
+    http::basic_request<T> request("http://boost.org/handler.php");
     request << header("Content-Type", "text/plain")
             << body("Hello, World!")
         ;
-    http::client::request copy(request);
+    http::basic_request<T> copy(request);
     BOOST_CHECK_EQUAL ( copy.host(), request.host() );
     BOOST_CHECK_EQUAL ( copy.port(), request.port() );
     BOOST_CHECK_EQUAL ( request.path(), request.path() );
     BOOST_CHECK_EQUAL ( body(request), body(copy) );
 }
 
-BOOST_AUTO_TEST_CASE(request_assignment_test) {
-    http::client::request request("http://boost.org/handler.php");
+BOOST_AUTO_TEST_CASE_TEMPLATE (request_assignment_test, T, tag_types) {
+    http::basic_request<T> request("http://boost.org/handler.php");
     request << header("Content-Type", "text/plain")
             << body("Hello, World!")
         ;
-    http::client::request copy;
+    http::basic_request<T> copy;
     copy = request;
     BOOST_CHECK_EQUAL ( copy.host(), request.host() );
     BOOST_CHECK_EQUAL ( copy.port(), request.port() );
@@ -50,9 +52,9 @@ BOOST_AUTO_TEST_CASE(request_assignment_test) {
     BOOST_CHECK_EQUAL ( body(request), body(copy) );
 }
 
-BOOST_AUTO_TEST_CASE(request_swap_test) {
-    boost::network::http::client::request request("http://boost.org/");
-    boost::network::http::client::request other;
+BOOST_AUTO_TEST_CASE_TEMPLATE (request_swap_test, T, tag_types) {
+    boost::network::http::basic_request<T> request("http://boost.org/");
+    boost::network::http::basic_request<T> other;
     swap(other, request); // ADL
     BOOST_CHECK_EQUAL ( request.host(), "" );
     BOOST_CHECK_EQUAL ( request.port(), 80u );
@@ -62,31 +64,31 @@ BOOST_AUTO_TEST_CASE(request_swap_test) {
     BOOST_CHECK_EQUAL ( other.path(), "/" );
 }
 
-BOOST_AUTO_TEST_CASE(response_constructor_test) {
-    http::client::response response;
+BOOST_AUTO_TEST_CASE_TEMPLATE (response_constructor_test, T, tag_types) {
+    http::basic_response<T> response;
     BOOST_CHECK_EQUAL ( body(response), std::string() );
 }
 
-BOOST_AUTO_TEST_CASE(response_copy_construct_test) {
-    http::client::response response;
+BOOST_AUTO_TEST_CASE_TEMPLATE (response_copy_construct_test, T, tag_types) {
+    http::basic_response<T> response;
     response.version() = "HTTP/1.1";
     response.status() = 200;
     response.status_message() = "OK";
     response << body("The quick brown fox jumps over the lazy dog");
-    http::client::response copy(response);
+    http::basic_response<T> copy(response);
     BOOST_CHECK_EQUAL ( response.version(), copy.version() );
     BOOST_CHECK_EQUAL ( response.status(), copy.status() );
     BOOST_CHECK_EQUAL ( response.status_message(), copy.status_message() );
     BOOST_CHECK_EQUAL ( body(response), body(copy) );
 }
 
-BOOST_AUTO_TEST_CASE(response_assignment_construct_test) {
-    http::client::response response;
+BOOST_AUTO_TEST_CASE_TEMPLATE (response_assignment_construct_test, T, tag_types) {
+    http::basic_response<T> response;
     response.version() = "HTTP/1.1";
     response.status() = 200;
     response.status_message() = "OK";
     response << body("The quick brown fox jumps over the lazy dog");
-    http::client::response copy;
+    http::basic_response<T> copy;
     copy = response;
     BOOST_CHECK_EQUAL ( response.version(), copy.version() );
     BOOST_CHECK_EQUAL ( response.status(), copy.status() );
@@ -94,13 +96,13 @@ BOOST_AUTO_TEST_CASE(response_assignment_construct_test) {
     BOOST_CHECK_EQUAL ( body(response), body(copy) );
 }
 
-BOOST_AUTO_TEST_CASE(response_swap_test) {
-    http::client::response response;
+BOOST_AUTO_TEST_CASE_TEMPLATE (response_swap_test, T, tag_types) {
+    http::basic_response<T> response;
     response.version() = "HTTP/1.1";
     response.status() = 200;
     response.status_message() = "OK";
     response << boost::network::body("RESPONSE");
-    boost::network::http::client::response swapped;
+    boost::network::http::basic_response<T> swapped;
     BOOST_REQUIRE_NO_THROW(swap(response, swapped));
     BOOST_CHECK_EQUAL ( response.version(), std::string() );
     BOOST_CHECK_EQUAL ( response.status(), 0u );
