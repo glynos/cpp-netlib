b44fffd942a2287dceed2a0bbed727222bed7524
Dean Michael Berris <mikhailberis@gmail.com>
2010-06-22 22:54:41 +0800

    Adding more support functions.

---
+++ boost/network/support/is_tcp.hpp
@@ -0,0 +1,26 @@
+#ifndef BOOST_NETWORK_SUPPORT_IS_TCP_HPP_20100622
+#define BOOST_NETWORK_SUPPORT_IS_TCP_HPP_20100622
+
+// Copyright 2010 (c) Dean Michael Berris
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/network/tags.hpp>
+#include <boost/type_traits/is_base_of.hpp>
+
+namespace boost { namespace network {
+
+    template <class Tag>
+    struct is_tcp :
+        is_base_of<
+            tags::tcp,
+            Tag
+        >
+    {};
+
+} // namespace network
+
+} // namespace boost
+
+#endif // BOOST_NETWORK_SUPPORT_IS_TCP_HPP_20100622
