483446412b723f60ec5ffa8e966b36b13fc13d22
Dean Michael Berris <dmberris@chikka.com>
2010-09-27 18:08:30 +0800

    Fixing license corruption and adding note to fix the tags file for later.

--- boost/network/tags.hpp
+++ boost/network/tags.hpp
@@ -1,12 +1,12 @@
 //          Copyright Dean Michael Berris 2008, 2009.
 //                    Glyn Matthews 2009
 // Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSEmpl::placeholders::_1_0.txt or copy at
-//          http://www.boost.org/LICENSEmpl::placeholders::_1_0.txt)
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
 
 
 #ifndef BOOST_NETWORK_TAG_INCLUDED_20100808
-# define BOOST_NETWORK_TAG_INCLUDED_20100808
+#define BOOST_NETWORK_TAG_INCLUDED_20100808
 
 #include <boost/mpl/vector.hpp>
 #include <boost/mpl/inherit.hpp>
@@ -25,14 +25,15 @@ namespace boost { namespace network { namespace tags {
     struct default_string { int unused; };
     struct default_wstring { int unused; };
 
+    // FIXME move out these HTTP-only tags into different files
+
     typedef mpl::vector<http, simple, sync, tcp, default_string> http_default_8bit_tcp_resolve_tags;
     typedef mpl::vector<http, simple, sync, udp, default_string> http_default_8bit_udp_resolve_tags;
     typedef mpl::vector<http, keepalive, sync, tcp, default_string> http_keepalive_8bit_tcp_resolve_tags;
     typedef mpl::vector<http, keepalive, sync, udp, default_string> http_keepalive_8bit_udp_resolve_tags;
-
-    typedef mpl::vector<http, simple, sync, default_string> http_server_tags;
     typedef mpl::vector<http, simple, async, udp, default_string> http_async_8bit_udp_resolve_tags;
     typedef mpl::vector<http, simple, async, tcp, default_string> http_async_8bit_tcp_resolve_tags;
+    typedef mpl::vector<http, simple, sync, default_string> http_server_tags;
 
     template <class Tag>
     struct components;
