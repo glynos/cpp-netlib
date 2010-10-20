3ba5ff9e40a6cd6cead38e9f639559ed5c25fa7c
Dean Michael Berris <mikhailberis@gmail.com>
2010-10-20 22:05:54 +0800

    Initial step to creating an asynchronous HTTP server.

--- boost/network/protocol/http/tags.hpp
+++ boost/network/protocol/http/tags.hpp
@@ -26,6 +26,7 @@ namespace boost { namespace network { namespace http { namespace tags {
     typedef mpl::vector<http, simple, async, udp, default_string> http_async_8bit_udp_resolve_tags;
     typedef mpl::vector<http, simple, async, tcp, default_string> http_async_8bit_tcp_resolve_tags;
     typedef mpl::vector<http, simple, sync, pod, default_string> http_server_tags;
+    typedef mpl::vector<http, simple, async, pod, default_string> http_async_server_tags;
 
     BOOST_NETWORK_DEFINE_TAG(http_default_8bit_tcp_resolve);
     BOOST_NETWORK_DEFINE_TAG(http_default_8bit_udp_resolve);
@@ -34,6 +35,7 @@ namespace boost { namespace network { namespace http { namespace tags {
     BOOST_NETWORK_DEFINE_TAG(http_async_8bit_udp_resolve);
     BOOST_NETWORK_DEFINE_TAG(http_async_8bit_tcp_resolve);
     BOOST_NETWORK_DEFINE_TAG(http_server);
+    BOOST_NETWORK_DEFINE_TAG(http_async_server);
 
 } /* tags */
     
