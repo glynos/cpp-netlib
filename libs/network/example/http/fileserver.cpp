12321508641ec6c18860b5a9f2b1b920c2f93e6a
Dean Michael Berris <mikhailberis@gmail.com>
2010-11-22 18:04:54 +0800

    Make the example listen to the default IP address (i.e. on all interfaces.)

--- libs/network/example/http/fileserver.cpp
+++ libs/network/example/http/fileserver.cpp
@@ -175,7 +175,7 @@ int main(int argc, char * argv[]) {
     file_cache cache(".");
     file_server handler(cache);
     utils::thread_pool thread_pool(4);
-    server instance("127.0.0.1", "8000", handler, thread_pool);
+    server instance("0.0.0.0", "8000", handler, thread_pool);
     instance.run();
     return 0;
 }
