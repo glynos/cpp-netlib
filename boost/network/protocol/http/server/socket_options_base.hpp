c3c69ad58c32838ce4e0bb177660df8dd28f9be2
Dean Michael Berris <mikhailberis@gmail.com>
2010-12-30 20:22:57 +0800

    Fixing Performance-Affecting Regression
    Apparently, with the introduction of settings for the receive_buffer,
    send_buffer, and send/receive low watermarks, responsiveness on
    previously responsive request handling suffers significantly based on
    local benchmarks leading to more timeouts from the client side.
    
    By making these options truly optional instead of making them actually
    default to some value reverses the regression.

--- boost/network/protocol/http/server/socket_options_base.hpp
+++ boost/network/protocol/http/server/socket_options_base.hpp
@@ -7,6 +7,8 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/network/protocol/http/server/parameters.hpp>
+#include <boost/optional.hpp>
+#include <boost/utility/in_place_factory.hpp>
 
 namespace boost { namespace network { namespace http {
     
@@ -14,10 +16,10 @@ namespace boost { namespace network { namespace http {
     protected:
         asio::socket_base::reuse_address acceptor_reuse_address;
         asio::socket_base::enable_connection_aborted acceptor_report_aborted;
-        asio::socket_base::receive_buffer_size receive_buffer_size;
-        asio::socket_base::send_buffer_size send_buffer_size;
-        asio::socket_base::receive_low_watermark receive_low_watermark;
-        asio::socket_base::send_low_watermark send_low_watermark;
+        boost::optional<asio::socket_base::receive_buffer_size> receive_buffer_size;
+        boost::optional<asio::socket_base::send_buffer_size> send_buffer_size;
+        boost::optional<asio::socket_base::receive_low_watermark> receive_low_watermark;
+        boost::optional<asio::socket_base::send_low_watermark> send_low_watermark;
         asio::socket_base::non_blocking_io non_blocking_io;
         asio::socket_base::linger linger;
 
@@ -25,13 +27,14 @@ namespace boost { namespace network { namespace http {
         socket_options_base(ArgPack const & args)
         : acceptor_reuse_address(args[_reuse_address|false])
         , acceptor_report_aborted(args[_report_aborted|false])
-        , receive_buffer_size(args[_receive_buffer_size|4096])
-        , send_buffer_size(args[_send_buffer_size|4096])
-        , receive_low_watermark(args[_receive_low_watermark|1024])
-        , send_low_watermark(args[_send_low_watermark|1024])
         , non_blocking_io(args[_non_blocking_io|true])
         , linger(args[_linger|true], args[_linger_timeout|0])
-        {}
+        {
+            set_optional(receive_buffer_size, args, _receive_buffer_size);
+            set_optional(send_buffer_size, args, _send_buffer_size);
+            set_optional(receive_low_watermark, args, _receive_low_watermark);
+            set_optional(send_low_watermark, args, _send_low_watermark);
+        }
 
         void acceptor_options(boost::asio::ip::tcp::acceptor & acceptor) {
             acceptor.set_option(acceptor_reuse_address);
@@ -42,11 +45,40 @@ namespace boost { namespace network { namespace http {
             boost::system::error_code ignored;
             socket.io_control(non_blocking_io, ignored);
             socket.set_option(linger, ignored);
-            socket.set_option(receive_buffer_size, ignored);
-            socket.set_option(receive_low_watermark, ignored);
-            socket.set_option(send_buffer_size, ignored);
-            socket.set_option(send_low_watermark, ignored);
+            if (receive_buffer_size) socket.set_option(*receive_buffer_size, ignored);
+            if (receive_low_watermark) socket.set_option(*receive_low_watermark, ignored);
+            if (send_buffer_size) socket.set_option(*send_buffer_size, ignored);
+            if (send_low_watermark) socket.set_option(*send_low_watermark, ignored);
         }
+
+    private:
+
+        template <class Optional, class Args, class Keyword>
+        typename boost::enable_if<
+            mpl::not_<
+                boost::is_same<
+                    typename boost::parameter::value_type<Args, Keyword, void>::type
+                    , void
+                    >
+                >
+            , void
+        >::type
+        set_optional(Optional & option, Args const & args, Keyword const & keyword) {
+            option = in_place<typename Optional::value_type>(args[keyword]);
+        }
+
+        template <class Optional, class Args, class Keyword>
+        typename boost::enable_if<
+            boost::is_same<
+                typename boost::parameter::value_type<Args, Keyword, void>::type
+                , void
+                >
+            , void
+        >::type
+        set_optional(Optional &, Args const &, Keyword const &) {
+            // do nothing
+        }
+
     };
 
 } /* http */
