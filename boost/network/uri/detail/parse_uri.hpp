ab45cedd030bf4b9d8db89f7df7206c87a013e7c
Dean Michael Berris <mikhailberis@gmail.com>
2011-03-06 18:15:47 +0800

    Supporting BOOST_NETWORK_NO_LIB
    This commit actually enables support for the BOOST_NETWORK_NO_LIB macro
    that will allow users of the library to define this so that there will
    be no need to have the externally-linked components when the network
    library is used. This commit only adds the support for the URI parsing
    parts -- this means that code using the HTTP client code don't need to
    link the URI parser external library to use it.
    
    NOTE: I'm still thinking about whether it's better that the default
    implementation would remain header-only, and only have a special macro
    for requiring the externally-linked libraries. I'm still not convinced
    that the default for header-only would be productive, but I'm still
    thinking about how to support some sort of backward compatibility for
    the code that's already using the library in header only mode.

--- boost/network/uri/detail/parse_uri.hpp
+++ boost/network/uri/detail/parse_uri.hpp
@@ -9,6 +9,9 @@
 #include <boost/network/uri/detail/uri_parts.hpp>
 #include <boost/network/support/is_default_string.hpp>
 #include <boost/range/iterator_range.hpp>
+#ifdef BOOST_NETWORK_NO_LIB
+#include <boost/network/uri/detail/impl/parse_uri.ipp>
+#endif
 
 namespace boost { namespace network { namespace uri { namespace detail {
 
@@ -17,8 +20,10 @@ inline bool parse_specific(uri_parts<Tag> & parts) {
     return true;
 }
 
+#ifndef BOOST_NETWORK_NO_LIB
 extern bool parse_uri_impl(boost::iterator_range<std::string::const_iterator> & range, uri_parts_default_base & parts, tags::default_string);
 extern bool parse_uri_impl(boost::iterator_range<std::wstring::const_iterator> & range, uri_parts_wide_base & parts, tags::default_wstring);
+#endif
 
 template <class Tag>
 struct unsupported_tag;
---
+++ libs/network/test/http/client_include_inlined.cpp
@@ -0,0 +1,17 @@
+// Copyright 2011 Dean Michael Berris &lt;mikhailberis@gmail.com&gt;. 
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#define BOOST_NETWORK_NO_LIB
+#include <boost/network/include/http/client.hpp>
+
+int main(int argc, char * argv[]) {
+    using namespace boost;
+    using namespace boost::network;
+    http::client c;
+    http::client::request req("http://www.boost.org/");
+    http::client::response res = c.get(req);
+    return 0;
+}
+
