5fa4a17ff654f919fbbddf445cac1ca55ad076a9
Glyn Matthews <glyn.matthews@gmail.com>
2012-02-26 07:31:47 +0100

    Added missing copyright notices.

--- boost/network/uri/directives/fragment.hpp
+++ boost/network/uri/directives/fragment.hpp
@@ -1,3 +1,9 @@
+//            Copyright (c) Glyn Matthews 2011, 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
 #ifndef __BOOST_NETWORK_URI_DIRECTIVES_FRAGMENT_INC__
 # define __BOOST_NETWORK_URI_DIRECTIVES_FRAGMENT_INC__
 
