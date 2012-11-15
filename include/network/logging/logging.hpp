47e8f868a53170690fcef0cae6992f1fa74c88e6
Klaim - A. Joel Lamotte <mjklaim@gmail.com>
2012-11-15 12:09:56 +0100

    Added missing copyright informations (klaim)

--- include/network/logging/logging.hpp
+++ include/network/logging/logging.hpp
@@ -1,3 +1,8 @@
+// Copyright 2011 A. Joel Lamotte <mjklaim@gmail.com>.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef NETWORK_LOGGING_HPP_20121112
 #define NETWORK_LOGGING_HPP_20121112
 
--- libs/network/src/logging/logging.cpp
+++ libs/network/src/logging/logging.cpp
@@ -1,3 +1,8 @@
+// Copyright 2011 A. Joel Lamotte <mjklaim@gmail.com>.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #ifdef NETWORK_NO_LIB
 #undef NETWORK_NO_LIB
 #endif
