fe870cb6788c46b82da0211c6411565d88da664f
Klaim - A. Joel Lamotte <mjklaim@gmail.com>
2012-11-15 12:05:14 +0100

    Fixed indentation.

--- include/network/logging/logging.hpp
+++ include/network/logging/logging.hpp
@@ -6,60 +6,60 @@
 
 namespace network { namespace logging { 
 
-	class log_record;
-	
-	//using log_record_handler = std::function< void (const std::string&) >; // use this when VS can compile it...
-	typedef std::function< void (const log_record&) > log_record_handler;
+class log_record;
 
-	void set_log_record_handler( log_record_handler handler );
-	void log( const log_record& message );
-	
-	namespace handler
-	{
-		log_record_handler get_std_log_handler();
-		log_record_handler get_default_log_handler();
-	}
-	
-	/** Helper to build a log record as a stream. */
-	class log_record
-	{
-	public:
-		log_record(){} // = default;
+//using log_record_handler = std::function< void (const std::string&) >; // use this when VS can compile it...
+typedef std::function< void (const log_record&) > log_record_handler;
 
-		// Implicit construction from string
-		log_record( const std::string& message ) 
-		{
-			write( message );
-		}
+void set_log_record_handler( log_record_handler handler );
+void log( const log_record& message );
 
-		~log_record()
-		{ 
-			log( *this );
-		}
-		
-		template< typename TypeOfSomething >
-		log_record& write( const TypeOfSomething& something ) // THINK: use universal references?
-		{
-			m_text_stream << something;
-			return *this;
-		}
+namespace handler
+{
+  log_record_handler get_std_log_handler();
+  log_record_handler get_default_log_handler();
+}
 
-		std::string full_message() const { return m_text_stream.str(); }
+/** Helper to build a log record as a stream. */
+class log_record
+{
+public:
+  log_record(){} // = default;
 
-	private:
-		// disable copy
-		log_record( const log_record& ); // = delete;
-		log_record& operator=( const log_record& ); // = delete;
+  // Implicit construction from string
+  log_record( const std::string& message ) 
+  {
+    write( message );
+  }
 
-		std::ostringstream m_text_stream; // stream in which we build the message
-			
-	};
-		
-	template< typename TypeOfSomething >
-	inline log_record& operator<<( log_record& log, const TypeOfSomething& something ) // THINK: use universal references?
-	{
-		return log.write( something );
-	}
+  ~log_record()
+  { 
+    log( *this );
+  }
+  
+  template< typename TypeOfSomething >
+  log_record& write( const TypeOfSomething& something ) // THINK: use universal references?
+  {
+    m_text_stream << something;
+    return *this;
+  }
+
+  std::string full_message() const { return m_text_stream.str(); }
+
+private:
+  // disable copy
+  log_record( const log_record& ); // = delete;
+  log_record& operator=( const log_record& ); // = delete;
+
+  std::ostringstream m_text_stream; // stream in which we build the message
+    
+};
+  
+template< typename TypeOfSomething >
+inline log_record& operator<<( log_record& log, const TypeOfSomething& something ) // THINK: use universal references?
+{
+  return log.write( something );
+}
 
 }}
 
--- libs/network/src/logging/logging.cpp
+++ libs/network/src/logging/logging.cpp
@@ -1,4 +1,3 @@
-
 #ifdef NETWORK_NO_LIB
 #undef NETWORK_NO_LIB
 #endif
@@ -10,43 +9,43 @@
 
 namespace network { namespace logging { 
 
-	namespace handler 
-	{
-		namespace
-		{
-			void std_log_handler( const log_record& log )
-			{
-				std::cerr << log.full_message() << std::endl;
-			}
-		}
+namespace handler 
+{
+  namespace
+  {
+    void std_log_handler( const log_record& log )
+    {
+      std::cerr << log.full_message() << std::endl;
+    }
+  }
 
-		log_record_handler get_std_log_handler() { return &std_log_handler; }
-		log_record_handler get_default_log_handler() { return &std_log_handler; }
-	}
+  log_record_handler get_std_log_handler() { return &std_log_handler; }
+  log_record_handler get_default_log_handler() { return &std_log_handler; }
+}
 
 
-	namespace 
-	{
-		// the log handler have to manage itself the thread safety on call
-		log_record_handler current_log_record_handler = handler::std_log_handler;
-		boost::upgrade_mutex mutex_log_handler; // we still need to not change the log handler concurrently
-	}
+namespace 
+{
+  // the log handler have to manage itself the thread safety on call
+  log_record_handler current_log_record_handler = handler::std_log_handler;
+  boost::upgrade_mutex mutex_log_handler; // we still need to not change the log handler concurrently
+}
 
-	
-	void set_log_record_handler( log_record_handler handler )
-	{
-		boost::lock_guard<boost::upgrade_mutex> write_lock( mutex_log_handler );
-		current_log_record_handler = handler;
-	}
+  
+void set_log_record_handler( log_record_handler handler )
+{
+  boost::lock_guard<boost::upgrade_mutex> write_lock( mutex_log_handler );
+  current_log_record_handler = handler;
+}
 
-	void log( const log_record& log )
-	{
-		boost::shared_lock<boost::upgrade_mutex> read_lock( mutex_log_handler );
-		if( current_log_record_handler )
-		{
-			current_log_record_handler( log );
-		}
-	}
+void log( const log_record& log )
+{
+  boost::shared_lock<boost::upgrade_mutex> read_lock( mutex_log_handler );
+  if( current_log_record_handler )
+  {
+    current_log_record_handler( log );
+  }
+}
 
 
 }}
