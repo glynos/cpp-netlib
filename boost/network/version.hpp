55d6d3764f80b6eaa797c979092df13497034f4d
Dean Michael Berris <dberris@google.com>
2013-02-05 22:00:24 +1100

    Updating version.

--- boost/network/version.hpp
+++ boost/network/version.hpp
@@ -1,17 +1,17 @@
 #ifndef BOOST_NETWORK_VERSION_HPP_20091214
 #define BOOST_NETWORK_VERSION_HPP_20091214
 
-//          Copyright Dean Michael Berris 2009.
-//          Copyright Glyn Matthews 2010.
+// Copyright 2009, 2013 Dean Michael Berris <dberris@google.com>
+// Copyright Glyn Matthews 2010.
 // Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          http://www.boost.org/LICENSE_1_0.txt)
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/config.hpp>
 
 #define BOOST_NETLIB_VERSION_MAJOR 0
 #define BOOST_NETLIB_VERSION_MINOR 9
-#define BOOST_NETLIB_VERSION_INCREMENT 0
+#define BOOST_NETLIB_VERSION_INCREMENT 5
 
 #ifndef BOOST_NETLIB_VERSION
 # define BOOST_NETLIB_VERSION \
