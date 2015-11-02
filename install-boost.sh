feec209a366e1b112d35a82f0828340984ef62ee
Dean Michael Berris <dberris@google.com>
2015-11-03 00:06:53 +1100

    Fix another typo on the URL.

--- install-boost.sh
+++ install-boost.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ ! -d "${HOME}/${CC}-boost_${BOOST_VERSION}/include" ]; then
-  wget -O boost_${BOOST_VERSION}.tar.bz2 http://sourceforge.get/projects/boost/files/boost/${BOOST_VER}/boost_${BOOST_VERSION}.tar.bz2/download
+  wget -O boost_${BOOST_VERSION}.tar.bz2 http://sourceforge.net/projects/boost/files/boost/${BOOST_VER}/boost_${BOOST_VERSION}.tar.bz2/download
   tar jxf boost_${BOOST_VERSION}.tar.bz2
   cd boost_${BOOST_VERSION}
   ./bootstrap.sh --with-toolset=$TOOLSET --prefix=${HOME}/${CC}-boost_${BOOST_VERSION}
