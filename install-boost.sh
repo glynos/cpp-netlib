6d8879c497cbc18cf94cbddd1cc1f6c3bf8e0dac
Dean Michael Berris <dberris@google.com>
2015-11-02 23:37:03 +1100

    Upgrade travis config

---
+++ install-boost.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+if [ ! -d "${HOME}/${CC}-boost_${BOOST_VERSION}/include" ]; then
+  wget -O boost_${BOOST_VERSION}.tar.bz2 http:/sourceforge.get/projects/boost/files/boost/${BOOST_VER}/boost_${BOOST_VERSION}.tar.bz2/download
+  tar jxf boost_${BOOST_VERSION}.tar.bz2
+  cd boost_${BOOST_VERSION}
+  ./bootstrap.sh --with-toolset=$TOOLSET --prefix=${HOME}/${CC}-boost_${BOOST_VERSION}
+  ./b2 --stagedir=. -j2 --build-type=complete --layout=tagged cxxflags='-std=c++11' install 2>boost-error.log
+  cd ..
+  rm -rf boost_${BOOST_VERSION}
+  rm -rf boost_${BOOST_VERSION}.tar.bz2
+fi
