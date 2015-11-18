9bad07c3d236ce6c8a25f7cf6abfb06c6bddd1de
Dean Michael Berris <dberris@google.com>
2015-11-18 23:29:51 +1100

    Removing noexcept from defaulted move constructor

--- boost/network/message.hpp
+++ boost/network/message.hpp
@@ -46,7 +46,7 @@ struct basic_message {
 
   basic_message() = default;
   basic_message(const basic_message&) = default;
-  basic_message(basic_message&&) noexcept = default;
+  basic_message(basic_message&&) = default;
   basic_message& operator=(basic_message const&) = default;
   basic_message& operator=(basic_message&&) = default;
   ~basic_message() = default;
